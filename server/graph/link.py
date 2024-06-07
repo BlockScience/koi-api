ffde3057c89cdfa083b81b94d766867d534cfa18
lukvmil <millerluke1364@gmail.com>
2024-06-07 18:26:56 +0800

    setup new link endpoints and functions, now a directed edge between a single source and target with no update funciton, implemented directly as an edge within neo4j

--- server/graph/link.py
+++ server/graph/link.py
@@ -2,14 +2,13 @@ from .utils import execute_read, execute_write
 from rid_lib import RID
 
 @execute_write
-def create(tx, rid: RID, tag, sources, targets):
+def create(tx, rid: RID, tag, source, target):
     CREATE_LINK = """
-        MERGE (r:link {rid: $rid})
-        SET r += $params
-        WITH r UNWIND $source_rids AS source_rid
-        MATCH (source {rid: source_rid})
-        MERGE (r)-[:SOURCE]->(source)
-        RETURN source.rid AS source
+        MATCH (source {rid: $source_rid})
+        MATCH (target {rid: $target_rid})
+        MERGE (source)-[link:LINK {rid: $rid}]->(target)
+        SET link += $params
+        RETURN link.rid AS link
         """
     
     params = {
@@ -17,82 +16,25 @@ def create(tx, rid: RID, tag, sources, targets):
         "tag": tag
     }
 
-    source_records = tx.run(CREATE_LINK, rid=str(rid), params=params, source_rids=sources)
-    sources = [record.get("source") for record in source_records]
+    link = tx.run(CREATE_LINK, rid=str(rid), params=params, source_rid=source, target_rid=target)
 
-    ADD_TARGETS = """
-        MATCH (r:link {rid: $rid})
-        UNWIND $target_rids AS target_rid
-        MATCH (target {rid: target_rid})
-        MERGE (r)-[:TARGET]->(target)
-        RETURN target.rid AS target
-        """
-    
-    target_records = tx.run(ADD_TARGETS, rid=str(rid), target_rids=targets)
-    targets = [record.get("target") for record in target_records]
-
-    return sources, targets
+    return
 
 @execute_read
 def read(tx, rid: RID):
-    READ_LINK_MEMBERS = """
-        MATCH (r:link {rid: $rid})-[e:SOURCE|TARGET]->(member)
-        RETURN TYPE(e) AS type, member.rid AS member
+    READ_LINK = """
+        MATCH (source)-[link:LINK {rid: $rid}]->(target)
+        RETURN source, target
         """
     
-    member_records = tx.run(READ_LINK_MEMBERS, rid=str(rid))
-    sources = []
-    targets = []
-
-    for record in member_records:
-        edge_type = record.get("type")
-        member = record.get("member")
-
-        if edge_type == "SOURCE":
-            sources.append(member)
-        elif edge_type == "TARGET":
-            targets.append(member)
-    
-    return sources, targets
+    record = tx.run(READ_LINK, rid=str(rid)).single()
+    return record["source"]["rid"], record["target"]["rid"]
 
 @execute_write
-def update(tx, rid: RID, add_sources, remove_sources, add_targets, remove_targets):
-    if add_sources:
-        ADD_SOURCES = """
-            MATCH (r:link {rid: $rid})
-            UNWIND $source_rids AS source_rid
-            MATCH (source {rid: source_rid})
-            MERGE (r)-[:SOURCE]->(source)
-            """
-
-        tx.run(ADD_SOURCES, rid=str(rid), source_rids=add_sources)
-
-    if add_targets:
-        ADD_TARGETS = """
-            MATCH (r:link {rid: $rid})
-            UNWIND $target_rids AS target_rid
-            MATCH (target {rid: target_rid})
-            MERGE (r)-[:TARGET]->(target)
-            """
-
-        tx.run(ADD_TARGETS, rid=str(rid), target_rids=add_targets)
-    
-    if remove_sources:
-        REMOVE_SOURCES = """
-            MATCH (r:link {rid: $rid})
-            UNWIND $source_rids AS source_rid
-            MATCH (r)-[edge:SOURCE]->(source {rid: source_rid})
-            DELETE edge
-            """
+def delete(tx, rid: RID):
+    DELETE_LINK = """
+        MATCH ()-[link:LINK {rid: $rid}]->()
+        DELETE link
+        """
         
-        tx.run(REMOVE_SOURCES, rid=str(rid), source_rids=remove_sources)
-
-    if remove_targets:
-        REMOVE_TARGETS = """
-            MATCH (r:link {rid: $rid})
-            UNWIND $target_rids AS target_rid
-            MATCH (r)-[edge:TARGET]->(target {rid: target_rid})
-            DELETE edge
-            """
-        
-        tx.run(REMOVE_TARGETS, rid=str(rid), target_rids=remove_targets)
+    tx.run(DELETE_LINK, rid=str(rid))
--- server/routers/link.py
+++ server/routers/link.py
@@ -12,19 +12,17 @@ router = APIRouter(
 
 class CreateLink(BaseModel):
     tag: str
-    sources: Optional[List[str]] = []
-    targets: Optional[List[str]] = []
+    source: str
+    target: str
 
 @router.post("")
 def create_link(rel: CreateLink):
     reference = rel.tag + "/" + nanoid.generate()
     rid = Link(reference)
-    sources, targets = graph.link.create(rid, rel.tag, rel.sources, rel.targets)
+    graph.link.create(rid, rel.tag, rel.source, rel.target)
 
     return {
-        "rid": str(rid),
-        "sources": sources,
-        "targets": targets
+        "rid": str(rid)
     }
 
 
@@ -34,53 +32,13 @@ class ReadLink(BaseModel):
 @router.get("")
 def read_link(rel: ReadLink):
     rid = RID.from_string(rel.rid)
-    check_existence(rid)
-
-    sources, targets = graph.link.read(rid)
+    source, target = graph.link.read(rid)
 
     return {
         "rid": str(rid),
-        "sources": sources,
-        "targets": targets
+        "source": source,
+        "target": target
     }
-
-
-class UpdateLink(BaseModel):
-    rid: str
-    add_sources: Optional[List[str]] = []
-    remove_sources: Optional[List[str]] = []
-    add_targets: Optional[List[str]] = []
-    remove_targets: Optional[List[str]] = []
-
-@router.put("")
-def update_link(rel: UpdateLink):
-    rid = RID.from_string(rel.rid)
-    check_existence(rid)
-
-    # remove duplicates
-    add_sources = set(rel.add_sources)
-    remove_sources = set(rel.remove_sources)
-    add_targets = set(rel.add_targets)
-    remove_targets = set(rel.remove_targets)
-
-    # remove intersections
-    source_intersection = add_sources & remove_sources
-    add_sources -= source_intersection
-    remove_sources -= source_intersection
-
-    target_intersection = add_targets & remove_targets
-    add_targets -= target_intersection
-    remove_targets -= target_intersection
-
-    graph.link.update(rid, list(add_sources), list(remove_sources), list(add_targets), list(remove_targets))
-    updated_sources, updated_targets = graph.link.read(rid)
-
-    return {
-        "rid": str(rid),
-        "sources": updated_sources, 
-        "targets": updated_targets
-    }
-
 
 class DeleteLink(BaseModel):
     rid: str
@@ -88,5 +46,4 @@ class DeleteLink(BaseModel):
 @router.delete("")
 def delete_link(rel: DeleteLink):
     rid = RID.from_string(rel.rid)
-    check_existence(rid)
-    graph.node.delete(rid)
+    graph.link.delete(rid)
