7ca726bb7117b1c4b28a20c6d3dbc376b111f0ee
lukvmil <millerluke1364@gmail.com>
2024-08-02 09:51:32 -0700

    updated setup

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
 setup (
-    name="koi_pond",
+    name="koi",
     version=0.1,
     url="https://github.com/BlockScience/koi-api/",
     author="Luke Miller",
     author_email="luke@block.science",
-    py_modules=["koi", "rid_lib"]
+    packages=[
+        "koi",
+        "rid_lib"
+    ]
 )
