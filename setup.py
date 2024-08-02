d6840d8754658817415c18302ce306772937e4ed
lukvmil <millerluke1364@gmail.com>
2024-08-02 15:27:28 -0700

    added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
 
-setup (
+def load_requirements():
+    with open("requirements.txt", "r") as f:
+        return [line.strip() for line in f if line.strip()]
+    
+setup(
     name="koi",
     version=0.1,
     url="https://github.com/BlockScience/koi-api/",
     author="Luke Miller",
     author_email="luke@block.science",
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=load_requirements()
 )
